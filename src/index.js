c64c014e407bdd45c9a7f1e33d60d2d5b60285d1
olexxxey <alexeykorneyevets@gmail.com>
2018-09-29 15:42:14 +0300

    porewal

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    if (currency <= 0) return {};
+    const half = { name: 'H', value: 50 };
+    const quarter = { name: 'Q', value: 25 };
+    const dime = { name: 'D', value: 10 };
+    const nickel = { name: 'N', value: 5 };
+    const penny = { name: 'P', value: 1 };
+    const coins = [half, quarter, dime, nickel, penny];
+    let rest = currency;
+    let result = {};
+    coins.forEach(({ name, value }) => {
+        const count = parseInt(rest/(value));
+        rest %= value;
+        if (count) result[name] = count;
+    });
+    return result;  
 }
